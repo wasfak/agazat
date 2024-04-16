11b97fe2a0f038f67b41dc85862cccfb6a785426
wasfak <hardmodz@yahoo.com>
2024-04-16 18:50:57 +0200

    agaza again

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 import { Button } from "@/components/ui/button";
 import prisma from "@/lib/db/prisma";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+
 export default async function Home() {
   const users = await prisma.user.findMany();
 
   return (
-    <main className="flex mt-10 justify-center min-h-screen gap-x-4">
-      {users.map((user) => (
-        <Button
-          key={user.id}
-          className={`${
-            user.isVac ? "bg-[#FF0000]" : "bg-[#008000]"
-          } text-white hover:bg-black`}
-        >
-          {user.username}
-        </Button>
-      ))}
+    <main className="flex flex-wrap mt-3 justify-center min-h-screen min-w-screen gap-4 ">
+      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-4 max-w-4xl max-h-[200px] p-4 border shadow-2xl rounded-lg bg-white">
+        {users.map((user) => (
+          <div className={`p-1`} key={user.id}>
+            <Button
+              className={`${user.isVac ? "bg-[#FF0000]" : "bg-[#008000]"}
+            text-white`}
+            >
+              {user.username}
+            </Button>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
